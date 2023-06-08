b493812309e929d92d63710c3c77e86cccc9307e
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-06-08 12:28:20 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, flash
+
+from functions import Counter
+
+app = Flask(__name__)
+app.secret_key = 'text mining'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        raw_text = request.files['doc']
+        
+        if not 'text' in raw_text.mimetype:
+            flash('Veuillez choisir un fichier texte')
+            return render_template('index.html')
+
+        text_lines = [line.decode() for line in raw_text]
+        
+        if len(text_lines) == 0:
+            flash('Veuillez choisir un fichier')
+            return render_template('index.html')
+        
+        counter = Counter(text_lines)
+        return render_template(
+            'index.html',
+            text=text_lines,
+            filename=raw_text.filename,
+            results={
+                "nombre d'espaces": counter.spaces(),
+                "nombre de mots": counter.words(),
+                "nombre de charactère": counter.characters(),
+                "keywords": counter.get_keywords(),
+                "nombres de phrases": counter.sentences(),
+                "verbs": counter.find_verbs(),
+            })
---
+++ functions.py
@@ -0,0 +1,96 @@
+from typing import List
+from collections import Counter as ctr
+
+import spacy
+import nltk
+
+
+class Counter:
+    def __init__(self, text: str):
+        self.text_list = text
+        self.raw_text_string = ' '.join(self.text_list)
+        self.keywords = []
+        self._load_stop_words()
+        self.verbs = []
+
+    def spaces(self) -> int:
+        result = 0
+        for line in self.text_list:
+            result += line.count(' ')
+        return result
+
+    def is_word(string: str, word) -> bool:
+        for char in word:
+            if char.isalnum():
+                return True
+        return False
+
+    def words(self) -> int:
+        return len([
+            word 
+            for line in self.text_list 
+            for word in line.split(' ')
+            if self.is_word(word)
+        ])
+
+    def characters(self) -> int:
+        return len([char for line in self.text_list for char in line])
+
+    def get_keywords(self) -> List[str]:
+        words = [
+            word
+            for line in self.text_list
+            for word in line.split()
+            if self.is_word(word) and word.lower() not in self.stopwords
+        ]
+        most_important_words = ctr(words)
+        for i, keyword in enumerate(most_important_words.most_common(8)):
+            keyword_value = keyword[0]
+            keyword_frequency = (keyword[1] / self.words()) * 100
+            self.keywords.append(
+                (keyword_value, round(keyword_frequency, 3), keyword[1])
+            )
+        print(self.keywords)
+        return self.keywords
+
+    def _load_stop_words(self):
+        # self.stopwords = set(nltk.corpus.stopwords.words('french'))
+        with open('french_stopwords') as f:
+            self.stopwords = [line.strip() for line in f]
+
+    def count_frequency(self) -> int:
+        pass
+
+    def sentences(self) -> int:
+        full_text = ' '.join(self.text_list)
+        return len(full_text.split('.'))
+
+    def paragraphs(self) -> int:
+        pass
+
+    def find_verbs(self)-> List[str]:
+        nlp = spacy.load('fr_core_news_sm')
+        tokens_from_spacy = nlp(self.raw_text_string)
+
+        self.verbs = [
+            word
+            for word in tokens_from_spacy
+            if word.pos_ == 'VERB'
+        ]
+
+        return self.verbs
+
+    def __repr__(self):
+        return str({
+            "spaces": self.spaces(),
+            "words": self.words(),
+            "characters": self.characters(),
+        })
+
+
+if __name__ == "__main__":
+    filename = 'sample.txt'
+    text_lines = [line.strip() for line in open(filename)]
+    counter = Counter(text_lines)
+    print(counter.is_word('..s.'))
+    print(counter)
